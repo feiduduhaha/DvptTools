335938700cfda2d9fa782552dc718d26e8a2abd6
silence <tianyun1201@163.com>
2020-07-23 15:33:57 +0800

    no message

--- DvptTools/DvptTools/MainView.swift
+++ DvptTools/DvptTools/MainView.swift
@@ -12,10 +12,10 @@ struct MainView: View {
     var body: some View {
         
         HStack {
-            List(0 ..< 5) { item in
+            List(0 ..< 30) { item in
                 Text("hello")
                     .font(.title)
-            }.frame(width: 300, height: 500)
+            }.frame(minWidth: 200, idealWidth: 200, maxWidth: 200, minHeight: 400, idealHeight: 1000, maxHeight: 1000, alignment: Alignment.topTrailing)
             DetailView().frame(width: 500, height: /*@START_MENU_TOKEN@*/100.0/*@END_MENU_TOKEN@*/)
         }
     }
@@ -23,7 +23,26 @@ struct MainView: View {
 
 struct DetailView: View {
     var body: some View {
-        Text("DetailView")
+        VStack {
+
+            Text("DetailView")
+            Button(action: {
+                
+                let panel = NSOpenPanel()
+                panel.allowsOtherFileTypes = false
+                panel.allowedFileTypes = ["xcodeproj"]
+                panel.beginSheetModal(for: NSApplication.shared.keyWindow ?? NSWindow()) { (modalResponse:NSApplication.ModalResponse) in
+                    
+                    if (modalResponse.rawValue == 1) {
+                        let fileUrl = panel.urls.first;
+                    }else{
+                        print("取消选择")
+                    }
+                }
+            }) {
+                Text("选择主工程")
+            }
+        }
     }
 }
 
